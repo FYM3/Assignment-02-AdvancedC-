aa7b43e04089c825418a4cd75c5c9572679f46c1
FYM3 <yousefmohame8i8@gmail.com>
2026-09-01 10:14:59 +0300

    Product Model

---
+++ Assignment#02-AdvancedC#/Product/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_02_AdvancedC_.product
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
--- Assignment#02-AdvancedC#/Program.cs
+++ Assignment#02-AdvancedC#/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Assignment_02_AdvancedC_
+﻿using Assignment_02_AdvancedC_.product;
+
+namespace Assignment_02_AdvancedC_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1) => Product Model
+
+            //Product product = new Product();
+
+            //product.Id = 1;
+            //product.Name = "Test";
+            //product.Price = 50.50;
+            //product.Stock = 1;
+            //product.Category = "Books";
+
+            //Console.WriteLine($"{product.Id}");
+            //Console.WriteLine($"{product.Name}");
+            //Console.WriteLine($"{product.Price}");
+            //Console.WriteLine($"{product.Stock}");
+            //Console.WriteLine($"{product.Category}");
+
+            #endregion
+
         }
     }
 }
